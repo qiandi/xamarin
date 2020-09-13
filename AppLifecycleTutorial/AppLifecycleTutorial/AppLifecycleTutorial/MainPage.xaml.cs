e3d03c33645ea475c49d53700ccfe25d97af3335
王豪 <wanghao1@sobey.com>
2020-09-13 23:10:33 +0800

    生命周期

---
+++ AppLifecycleTutorial/AppLifecycleTutorial/AppLifecycleTutorial/MainPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace AppLifecycleTutorial
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            entry.Text = (Application.Current as App).DisplayText;
+        }
+
+        private void OnEntryCompleted(object sender, EventArgs e)
+        {
+            (Application.Current as App).DisplayText = entry.Text;
+        }
+    }
+}
