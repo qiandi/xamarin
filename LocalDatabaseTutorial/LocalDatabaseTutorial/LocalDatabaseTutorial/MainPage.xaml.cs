362043a919d08eb7dd8ae2540081a7f654ccaf8e
王豪 <wanghao1@sobey.com>
2020-09-15 21:27:51 +0800

    sqlite使用

---
+++ LocalDatabaseTutorial/LocalDatabaseTutorial/LocalDatabaseTutorial/MainPage.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace LocalDatabaseTutorial
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        protected override async void OnAppearing()
+        {
+            base.OnAppearing();
+            listView.ItemsSource = await App.Database.GetPeopleAsync();
+        }
+
+        private async void OnButtonClicked(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrWhiteSpace(nameEntry.Text) && !string.IsNullOrWhiteSpace(ageEntry.Text))
+            {
+                await App.Database.SavePersonAsync(new Person
+                {
+                    Name = nameEntry.Text,
+                    Age = int.Parse(ageEntry.Text)
+                });
+
+                nameEntry.Text = ageEntry.Text = string.Empty;
+                listView.ItemsSource = await App.Database.GetPeopleAsync();
+            }
+        }
+    }
+}
---
+++ LocalDatabaseTutorial/LocalDatabaseTutorial/LocalDatabaseTutorial/Person.cs
@@ -0,0 +1,15 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LocalDatabaseTutorial
+{
+    public class Person
+    {
+        [PrimaryKey, AutoIncrement]
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+}
